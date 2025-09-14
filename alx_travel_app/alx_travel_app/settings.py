e757ff4d6f5bd0f2592416b4f3c76950d07b2e0e
ugochukwuemeka <chukwuemekaugochukwu00@gmail.com>
2025-09-14 03:46:58 +0100

    Update settings.py for PythonAnywhere deployment

--- alx_travel_app/alx_travel_app/settings.py
+++ alx_travel_app/alx_travel_app/settings.py
@@ -32,7 +32,7 @@ CHAPA_BASE_URL = env("CHAPA_BASE_URL", default="https://api.chapa.co/v1")
 
 DEBUG = env.bool("DEBUG", default=False)
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["ugochukwu.pythonanywhere.com"]
 
 
 # Application definition
@@ -134,7 +134,12 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+STATIC_ROOT = BASE_DIR / 'static'
+
+# Media files (uploads)
+MEDIA_URL = '/media/'
+MEDIA_ROOT = BASE_DIR / 'media'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
